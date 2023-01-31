ed7e54d05e90523ed1cf72bf63d6456f08678a69
Kauan-Almeida <kauanalmeida2233@gmail.com>
2023-01-31 13:18:36 -0300

    Predicate (Expressão lambda declarada)

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 import util.ProductPredicate;
@@ -18,9 +19,11 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
-		list.add(new Product("HD Case", 80.90))
-		;
-		list.removeIf(Product::testPredicateNoStatic);
+		list.add(new Product("HD Case", 80.90));
+		
+		Predicate<Product> pred = p -> p.getPrice() >= 100.00;
+		
+		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p);
